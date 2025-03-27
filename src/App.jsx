60d3ee0748bbf111691a0bec4547924f115d9766
celisawalker <celisa.walker@gmail.com>
2025-03-27 13:39:58 -0700

    imports and utlizes weather services function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
+import * as weatherService from "./services/weatherService.js"
 import './App.css'
 
 const App = () => {
-
+  const fetchData = async () => {
+      const data = await weatherService.show("London");
+      console.log("Data:", data);
+  }
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Weather API</h1>
+      <button onClick={fetchData}>Fetch Weather Data</button>
+    </>
   );
 }
 
--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,4 +1,4 @@
-const API_KEY = "";
+const API_KEY = "5d2c9f4894c449b2a6a184310252703";
 const BASE_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}`;
 
 const show = async (city) => {
